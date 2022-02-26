ec125d627e7cbdd2a4e8a5c389d286ae69b71ab7
Rohin Madhavan <rohinm@inzodiac.com>
2022-02-26 21:15:17 +0000

    Initial Commit

---
+++ Inzodiac_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inzodiac_iOS
+//
+//  Created by Rohin Madhavan on 26/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
